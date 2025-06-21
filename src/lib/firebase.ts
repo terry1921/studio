5e0d69cc3377eb37fd39971caf0d481a8fdb68f7
Enrique Espinoza “Terry0022” <terryrockstar22@gmail.com>
2025-06-21 19:01:45 +0000

    Add Realtime Database support to register user data like name, mail, rol

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -14,5 +15,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
+const db = getDatabase(app);
 
-export { app, auth, googleProvider };
+export { app, auth, db, googleProvider };
